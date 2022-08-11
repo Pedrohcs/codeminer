0b040db26c44518df725f561b93bb9cef67228ab
Pedro Henrique <pedro.hc.silva@outlook.com>
2022-08-11 14:05:32 -0300

    [003] Add validation

---
+++ app/utils/utils.js
@@ -0,0 +1,24 @@
+module.exports.validateCertification = function(certification) {
+    let evenSum = 0
+    let oddSum = 0
+    let char
+
+    for(let evenPosition = certification.length - 2; evenPosition >= 0; evenPosition = evenPosition - 2) {
+        char = parseInt(certification.charAt(evenPosition) + "")
+        evenSum = evenSum + sum(char * 2)
+    }
+
+    for(let oddPosition = certification.length - 1; oddPosition >= 0; oddPosition = oddPosition - 2) {
+        char = parseInt(certification.charAt(oddPosition) + "")
+        oddSum = oddSum + char
+    }
+
+    return (evenSum + oddSum) % 10 === 0
+}
+
+function sum(number) {
+    if(number < 9)
+        return number
+    else
+        return number % 10 + 1
+}
